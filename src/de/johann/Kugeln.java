6c3a7ebb6ed19a814e2421f50b5dea8e317184a1
Johann Schneider <joschi.dev7@gmail.com>
2025-10-30 09:50:51 +0100

    unnecessary code deleted

--- src/de/johann/Kugeln.java
+++ src/de/johann/Kugeln.java
@@ -25,41 +25,5 @@ public class Kugeln {
                 System.out.println("Fehler beim Erstellen des Kreises mit dem Radius " + radien[i] + ": " + e.getMessage());
             }
         }
-
-        /* 
-        Kugel kugel5 = new Kugel(5.0);
-
-        
-        
-        
-
-
-
-        Kugel kugel7 = new Kugel(7.0);
-        
-        String informationKugel7 = "Kugel mit Radius = %.2f [LE], Durchmesser = %.2f [LE], Volumen = %.2f [LE³], Oberfläche = %.2f [LE²]%n"
-        .formatted(
-            kugel7.getRadius(),
-            kugel7.getDurchmesser(), 
-            kugel7.getVolumen(),
-            kugel7.getOberflaeche() 
-        );
-
-        System.out.printf(informationKugel7);
-
-
-
-        Kugel kugelm21 = new Kugel(-21.0);
-
-        String informationKugelm21 = "Kugel mit Radius = %.2f [LE], Durchmesser = %.2f [LE], Volumen = %.2f [LE³], Oberfläche = %.2f [LE²]%n"
-        .formatted(
-            kugelm21.getRadius(),
-            kugelm21.getDurchmesser(), 
-            kugelm21.getVolumen(),
-            kugelm21.getOberflaeche()
-        );
-
-        System.out.printf(informationKugelm21);
-        */
     }
 }
